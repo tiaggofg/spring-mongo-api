3f1a3fcfaa9f57360e2f4a756b2e754ae25c096d
Tiago <tiago.fernandes.godoy@gmail.com>
2022-12-05 12:41:49 -0300

    Created project

---
+++ src/main/java/com/dev/spm/api/SpringMongoApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/spm/api/SpringMongoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
