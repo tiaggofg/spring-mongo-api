24a1957e9f727914689ace327b46b5a7a2c009b9
Tiago <tiago.fernandes.godoy@gmail.com>
2022-12-06 20:00:48 -0300

    Instantiated initial datas in the database

---
+++ src/main/java/com/dev/spm/api/config/Config.java
@@ -0,0 +1,27 @@
+package com.dev.spm.api.config;
+
+import com.dev.spm.api.domain.User;
+import com.dev.spm.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Config implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... arg) {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
