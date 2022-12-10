e39d77ae46612ee709c65376cf6347c9c7c33e75
Tiago <tiago.fernandes.godoy@gmail.com>
2022-12-09 23:18:21 -0300

    Implemented an endpoint to find post by id

---
+++ src/main/java/com/dev/spm/api/resources/PostResource.java
@@ -0,0 +1,24 @@
+package com.dev.spm.api.resources;
+
+import com.dev.spm.api.domain.Post;
+import com.dev.spm.api.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    PostService postService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/dev/spm/api/services/PostService.java
@@ -0,0 +1,21 @@
+package com.dev.spm.api.services;
+
+import com.dev.spm.api.domain.Post;
+import com.dev.spm.api.repositories.PostRepository;
+import com.dev.spm.api.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post id: " + id + " não encontrado!"));
+    }
+}
